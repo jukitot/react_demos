e33e937c6a39de14fbae3073a3dc14f4ffe1f992
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-19 09:49:15 +0300

    lesson 1-4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  //hook useState
+  // const [users, setUsers] = useState([])
+
+  //hook useEffect один раз вивод
+ // useEffect(()=>{
+ //   fetch('https://jsonplaceholder.typicode.com/users')
+ //       .then(value => value.json())
+ //       .then(value => {
+ //         setUsers(value);
+ // }, [])
+
+      // })
+  let [counter, setCounter] = useState(0)
+  const onClickIncrement = () => {
+      counter++;
+      setCounter(counter)
+  }
+  const onClickDecrement = () =>{
+      counter--;
+      setCounter(counter)
+  }
   return (
     <div className="App">
+
+        <h1>counter: {counter} </h1>
+        <button onClick={onClickIncrement}>+</button>
+        <button onClick={onClickDecrement}>-</button>
+
+        {/*// users.map(value => <div key={value.id}>{value.name} </div>)*/}
+
 
     </div>
   );
