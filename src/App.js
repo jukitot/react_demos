c76096e39f8140ec3632fd6c96be8cb64a9dfbee
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-23 11:15:16 +0300

    lesson2-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from "react";
+import {getAllUsers, getUserById} from "./services/user.api.service";
 function App() {
+
+  useEffect(()=>{
+    getAllUsers().then(value => console.log(value));
+    getAllUsers().then(value => console.log(value.data))
+
+    getUserById(9).then(value => console.log(value))
+    getUserById(9).then(value => console.log(value.data))
+  },[])
+
   return (
     <div className="App">
 
