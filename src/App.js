a7c3710284fccc5b0cb10ca5051a19ad7b77c742
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-25 09:40:40 +0300

    lesson3-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
 
 function App() {
+  // let [login, setLogin] = useState('default');
+  // let [password, setPassword] = useState('');
+  //
+  // function onLoginChange(e) {
+  //   setLogin(e.target.value)
+  // }
+  //
+  // function onPasswordChange(e) {
+  //   setPassword(e.target.value)
+  // }
+
+  let [formState, setFormState] = useState({login: '', password: ''})
+
+  function onSubmit(e){
+    console.log(e.target.login.value);
+    console.log(e.target.password.value);
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  function onChange(e){
+    setFormState({...formState, [e.target.name]:e.target.value})
+  }
+
   return (
     <div className="App">
+
+      <form onSubmit={onSubmit}>
+        <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
+        <input type="text" name={'password'} value={formState.password} onChange={onChange}/>
+        <button>Submit</button>
+      </form>
 
     </div>
   );
