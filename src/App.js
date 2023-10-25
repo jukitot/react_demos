c40a005efce13ca911fe315d7835c889c32e9e12
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-25 10:10:47 +0300

    lesson3-3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useForm} from "react-hook-form";
 
 function App() {
+  let {register, handleSubmit} = useForm();
+  const onSubmit = (data) =>{
+    console.log(data);
+  }
   return (
     <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('title', {required: true})}/>
+        <input {...register('body')}/>
+        <button>Save Post</button>
+      </form>
 
     </div>
   );
