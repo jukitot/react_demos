062b0cf8b70b1e9bcad27dd5e876c44964b3d430
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-25 10:27:37 +0300

    lesson3-4

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import './App.css';
 import {useForm} from "react-hook-form";
 
 function App() {
-  let {register, handleSubmit} = useForm();
+  let {register,
+    handleSubmit,
+    watch,
+    formState:{errors}} = useForm({defaultValues:{title:'title default'}});
   const onSubmit = (data) =>{
     console.log(data);
   }
+  console.log(watch('title'));
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register('title', {required: true})}/>
+        {errors.title && <span>field is required</span>}
         <input {...register('body')}/>
         <button>Save Post</button>
+
+        <select {...register('userId')}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
       </form>
 
     </div>
