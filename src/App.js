c01c0ff51f03e3b2a97aa34a94a03b4a60236095
Anastasia Irzhyckaya <57043957+anastasiaromanovna33@users.noreply.github.com>
2023-10-25 09:53:26 +0300

    lesson3-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {createRef} from "react";
 
 function App() {
+    let login = createRef();
+    let password = createRef();
+    function onSubmit(e){
+        console.log(login.current.value);
+        console.log(password.current.value);
+        e.preventDefault();
+    }
   return (
     <div className="App">
+      <form onSubmit={onSubmit}>
+          <input type="text" name={'login'} ref={login}/>
+          <input type="text" name={'password'} ref={password}/>
+          <button>Log in</button>
+      </form>
 
     </div>
   );
